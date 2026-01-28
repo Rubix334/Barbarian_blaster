a36b01c4058e1e3cf98030e4dece3eb8e8af2867
Rubix334 <2260334@flourbluffschools.org>
2026-01-28 12:45:25 -0600

    add game end

--- difficulty_manager.gd
+++ difficulty_manager.gd
@@ -5,12 +5,16 @@ extends Node
 @export var game_length := 30
 @export var enemy_health_curve : Curve
 
+signal stop_spawning_enemies
+
 func _ready() -> void:
 	timer.wait_time = game_length
 	timer.start()
+	Engine.time_scale = 5
 	
 func _process(delta: float) -> void:
-	print(get_spawn_time())
+	#print(get_spawn_time())
+	pass
 
 func game_progress_ratio() -> float:
 	return 1.0 - (timer.time_left / game_length)
@@ -20,3 +24,7 @@ func get_spawn_time() -> float:
 	
 func get_enemy_health() -> float:
 	return enemy_health_curve.sample(game_progress_ratio())
+
+
+func _on_timer_timeout() -> void:
+	stop_spawning_enemies.emit()
